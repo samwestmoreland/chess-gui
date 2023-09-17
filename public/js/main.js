94ae64657506a0e3178be90f2447205bdd4ce254
Sam Westmoreland <samwestmoreland@gmail.com>
2023-09-17 21:02:58 +0100

    Seem to have got the import working

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
 // Import the Chessboard.js library
-const Chessboard2 = require('@chrisoakman/chessboard2/dist/chessboard2.min.js');
+import { Chessboard2 } from "@chrisoakman/chessboard2/dist/chessboard2.min.js";
 
-// Initialize the chessboard
-const board = Chessboard2('myBoard')
+const board = Chessboard2('board2', 'start')
 
-// Example: Add an event listener to handle moves
-board.on('square_click', (square) => {
-    // This function will be called when a square is clicked.
-    console.log(`Square clicked: ${square}`);
-});
+window.setTimeout(makeRandomMove, 500)
+
+function makeRandomMove () {
+  if (game.game_over()) return
+
+  const legalMoves = game.moves()
+  const randomIdx = Math.floor(Math.random() * legalMoves.length)
+  game.move(legalMoves[randomIdx])
+  board.position(game.fen())
+
+  window.setTimeout(makeRandomMove, 500)
+}
